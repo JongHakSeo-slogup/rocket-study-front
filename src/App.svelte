cc4cf7bd03b7b413b0e2abbc5ed57e8c4c9363f8
seojonghak <unqocn@gmail.com>
2021-07-01 23:28:36 +0900

    init project

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Router from 'svelte-spa-router'
+	import { Styles } from 'sveltestrap';
+	import { routes } from "./routes";
+
+	export let title;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+</svelte:head>
+
+<Styles/>
+
+<body>
+<nav>
+	<a href="/">처음으로</a>
+	<a href="#/signup">회원가입</a>
+	<a href="#/about">about</a>
+</nav>
+<Router {routes}/>
+</body>
---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+import Home from "../pages/Home/Home.svelte"
+import About from "../pages/About/About.svelte"
+import SignUp from "../pages/SignUp/Signup.svelte"
+
+export const routes = {'/': Home, '/signup': SignUp,'/about': About }
